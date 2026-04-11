c8bdf17f9ab208b8fc74720d2dfd566f5c85cbfc
Mu_Cloud <Mu_Cloud@outlook.de>
2026-04-11 23:35:13 +0800

    [MuCore|WIP] Refine MCJEServer: Diversify the implements of MCJEServer | 优化 MCJEServer：分化其实现 [MuCore|EXP] Add the CREATED Status in ServerStatus | 在 ServerStatus 添加一个 CREATED 状态

--- MuCore/src/main/kotlin/me/mucloud/application/mk/serverlauncher/muserver/ServerStatus.kt
+++ MuCore/src/main/kotlin/me/mucloud/application/mk/serverlauncher/muserver/ServerStatus.kt
@@ -1,12 +1,13 @@
 package me.mucloud.application.mk.serverlauncher.muserver
 
 enum class ServerStatus(val code: Int) {
-    STOPPED(0),
-    PREPARING(1),
-    RUNNING(2),
-    STOPPING(3),
-    RESTARTING(4),
-    IN_ERROR(5);
+    CREATED(0),
+    STOPPED(1),
+    PREPARING(2),
+    RUNNING(3),
+    STOPPING(4),
+    RESTARTING(5),
+    IN_ERROR(6);
 
     override fun toString(): String = code.toString()
 }
