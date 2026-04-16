caca51764d9d7eeb98f64c138efb31a69f41d016
Mu_Cloud <Mu_Cloud@outlook.de>
2026-04-16 19:57:16 +0800

    [MuCore] Adjust the MuServerStatusPacket | 调整 MuServerStatusPacket

--- MuCore/src/main/kotlin/me/mucloud/application/mk/serverlauncher/mupacket/mucore/muserver/MuServerStatusPacket.kt
+++ MuCore/src/main/kotlin/me/mucloud/application/mk/serverlauncher/mupacket/mucore/muserver/MuServerStatusPacket.kt
@@ -2,13 +2,15 @@ package me.mucloud.application.mk.serverlauncher.mupacket.mucore.muserver
 
 import com.google.gson.JsonObject
 import me.mucloud.application.mk.serverlauncher.muserver.MCJEServer
+import me.mucloud.application.mk.serverlauncher.muserver.ServerStatus
 
 class MuServerStatusPacket(
     targetServer: MCJEServer,
+    val newStatus: ServerStatus,
 ): MuServerPacket(object : MuServerPacketInfo("status") {
     override fun fromData(data: JsonObject): MuServerStatusPacket = throw UnsupportedOperationException("MuServerStatusPacket not supported send to MuPacketReceiver, it should be send to MuView")
 }, targetServer) {
     override fun getMSPData(): JsonObject = JsonObject().apply {
-        addProperty("MSS", targetServer.mss.code)
+        addProperty("MSS", newStatus.code)
     }
 }
